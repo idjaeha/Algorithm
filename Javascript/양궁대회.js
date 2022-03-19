75dc9fc0edffb97178426e1acef6f324670d538e
idjaeha <jaehanim1@gmail.com>
2022-03-19 15:46:32 +0000

    양궁 대회 70% 완성

--- Javascript/양궁대회.js
+++ Javascript/양궁대회.js
@@ -25,7 +25,71 @@ https://programmers.co.kr/learn/courses/30/lessons/92342?language=javascript
 2. 남은 화살 개수로 어피치의 화살이 없는 경우를 채움.
 */
 
+// info에서 n개를 뽑아 answers에 저장하는 함수
+const getScores = (n, info, answers, lionScores, maxGap, apeachScores) => {
+  // 탈출 조건
+  if (n === 0 || info.length === 0) {
+    if (info.length === 0) {
+      lionScores[lionScores.length - 1] += n;
+    }
+    const gap = lionScores.reduce((prev, cur, idx) => {
+      // 라이온이 점수를 얻는 경우
+      if (cur > apeachScores[idx]) return prev + 10 - idx;
+      // 어피치가 점수를 얻는 경우
+      else if (apeachScores[idx] > 0) return prev - (10 - idx);
+      // 둘다 0개라서 점수를 못 얻는 경우
+      else return prev;
+    }, 0);
+    if (gap >= maxGap[0]) {
+      if (gap !== maxGap[0]) answers.length = 0;
+      maxGap[0] = gap;
+      answers.push(lionScores);
+    }
+    return;
+  }
+
+  // 본 로직
+  info.forEach((elem, idx) => {
+    // 남아있는 n이 elem보다 크면 elem + 1을 선택한다.
+    if (n > elem && elem > 0) {
+      getScores(
+        n - (elem + 1),
+        [...info.slice(idx + 1)],
+        answers,
+        [...lionScores.slice(0, idx), elem + 1, ...lionScores.slice(idx + 1)],
+        maxGap,
+        apeachScores
+      );
+    }
+    // 0을 만났을 경우에는 1을 선택한다.
+    if (elem === 0) {
+      getScores(
+        n - 1,
+        [...info.slice(idx + 1)],
+        answers,
+        [...lionScores.slice(0, idx), 1, ...lionScores.slice(idx + 1)],
+        maxGap,
+        apeachScores
+      );
+    }
+    // 아무것도 선택하지 않는 경우에는 0을 선택한다.
+    getScores(
+      n,
+      [...info.slice(idx + 1)],
+      answers,
+      [...lionScores.slice(0, idx), 0, ...lionScores.slice(idx + 1)],
+      maxGap,
+      apeachScores
+    );
+  });
+};
+
 function solution(n, info) {
-  let maxScore = -1;
-  const scores = [];
+  let maxGap = [-1];
+  const answers = [];
+
+  getScores(n, info, answers, new Array(11).fill(0), maxGap, info);
+  console.log(maxGap[0], answers);
 }
+
+solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]);
