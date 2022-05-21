d9693a483b4624a21e92338fab26e57c02c27a25
idjaeha <jaehanim1@gmail.com>
2022-05-21 15:05:31 +0900

    징검다리 건너기 변수명 변경

--- Javascript/징검다리_건너기.js
+++ Javascript/징검다리_건너기.js
@@ -1,44 +1,44 @@
 /*
 
-	1. 연속된 길이의 합이 길면 mid = start + 1
-	2. 연속된 길이의 합이 짧으면 mid = end
-	3. mid = (start + end) / 2
+	1. 연속된 길이의 합이 길면 midCnt = minCnt + 1
+	2. 연속된 길이의 합이 짧으면 midCnt = maxCnt
+	3. midCnt = (minCnt + maxCnt) / 2
 
 */
 
-function solution(stones, k) {
-  let start = 200000001; // 건널 수 있는 최소 값
-  let end = 0; // 건널 수 있는 최대 값
-  let mid; // 건널 수 있는 친구들을 찾기 위한 값
+function solution(stones, maxMoveLen) {
+  let minCnt = 200000001; // 건널 수 있는 최소 값
+  let maxCnt = 0; // 건널 수 있는 최대 값
+  let midCnt; // 건널 수 있는 친구들을 찾기 위한 값
 
   stones.forEach((x) => {
-    if (x < start) start = x;
-    if (x > end) end = x;
+    if (x < minCnt) minCnt = x;
+    if (x > maxCnt) maxCnt = x;
   });
 
-  while (start < end) {
-    mid = Math.ceil((start + end) / 2);
+  while (minCnt < maxCnt) {
+    midCnt = Math.ceil((minCnt + maxCnt) / 2);
 
-    let maxLength = 0;
-    let curLength = 0;
+    let maxBrokenLen = 0;
+    let brokenLen = 0;
 
-    for (let idx = 0; idx < stones.length; idx++) {
-      if (stones[idx] < mid) {
-        curLength++;
-        maxLength = Math.max(curLength, maxLength);
-        if (maxLength === k) break;
+    for (const stone of stones) {
+      if (stone < midCnt) {
+        brokenLen++;
+        maxBrokenLen = Math.max(brokenLen, maxBrokenLen);
+        if (maxBrokenLen === maxMoveLen) break;
       } else {
-        curLength = 0;
+        brokenLen = 0;
       }
     }
 
-    if (maxLength >= k) {
-      // k 보다 maxLength가 크면 건널 수 없음을 의미함.
-      end = mid - 1;
+    if (maxBrokenLen >= maxMoveLen) {
+      // maxMoveLen 보다 maxLength가 크면 건널 수 없음을 의미함.
+      maxCnt = midCnt - 1;
     } // k보다 maxLength가 작거나 같으면 건널 수 있음을 의미함.
     else {
-      start = mid;
+      minCnt = midCnt;
     }
   }
-  return start;
+  return minCnt;
 }
